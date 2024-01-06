c779ff1e7be980c90ddb2d7779b8a2937c5af0f7
Dilkhush Roy <58264438+Dilkhush7134@users.noreply.github.com>
2024-01-06 17:31:19 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd /tmp/project1
-docker build -t mynginxproject:v1 . &
+docker build -t projectos . &
 wait
 docker images
+docker tag projectos dilkhush7134/projectme:v1
 docker login &
 wait
-docker push dilkhush7134/mynginxproject:v1
+docker push dilkhush7134/projectme:v1
 docker images
